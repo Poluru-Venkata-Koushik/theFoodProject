e499560e83fad727841bdff1cd7e1703bbafe420
Poluru-Venkata-Koushik <poluru.vk@gmail.com>
2025-05-27 21:08:28 +0530

    creating dbStuff.py and frontend folder

---
+++ backend/dbStuff.py
@@ -0,0 +1,8 @@
+
+# This is the theFoodProject backend database module.
+# It contains functions to interact with the database, such as creating tables,
+# inserting data, and querying data.
+
+# Copyright (c) 2023, theFoodProject. All rights reserved.
+
+# We will be using MongoDB as our database. Local Instance for the moment, will move to Atlas later.
